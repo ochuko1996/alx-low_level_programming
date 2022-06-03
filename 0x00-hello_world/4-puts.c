9734124b078c53de6c187bd1228dc20cc94e13a5
Ochuko George <georgeochuko7@gmail.com>
2022-06-03 16:04:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
-include <stdio.h>
+#include <stdio.h>
+
+#include <string.h>
+
+
 
 /**
  *
- * * main - Write '"Programming is like building a multilingual puzzle'
+ * *main - Main function of my project
  *
- * *Return: Always 0 (success)
+ * *
+ *
+ * *
+ *
+ * *Return: return 0 everytime
  *
  * */
 
@@ -12,8 +20,16 @@ int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+
+		char displayed[52];
+
+
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+
+				puts(displayed);
+
+					return (0);
 
 }
