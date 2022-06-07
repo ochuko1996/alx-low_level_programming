d0736cde9b6ce5d5ca4e23ec6756839a86c8480a
Ochuko George <georgeochuko7@gmail.com>
2022-06-06 21:10:52 -0700

    last random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,6 +26,5 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, last);
 	else if (last < 6)
 		printf("Last digit of %i is %i and is less than 6 and not zero\n", n, last);
-		return (0);
-
+	return (0);
 }
